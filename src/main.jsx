498677c21bc1a6437e9a193c3777b0c28f4e2820
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-12 07:01:23 -0800

    force ci build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+// v0.0.1 - force ci to build
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
