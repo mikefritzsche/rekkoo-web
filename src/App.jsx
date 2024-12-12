75c71272ed1dc63a8d22ed7e3f050d0952d2423d
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-12 10:53:15 -0800

    make change

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <>
+      <div>Staging - staging-cleanup</div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
