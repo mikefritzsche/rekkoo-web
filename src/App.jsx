bf17fea1df75a60751af8395f3ed602026eeebd9
Mike Fritzsche <mike@mikefritzsche.com>
2024-12-12 09:29:40 -0800

    production head title

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import useTitle from "./hooks/useTitle.js";
 // v0.0.1
 function App() {
-  useTitle('Rekkoo::Staging')
+  useTitle('Rekkoo::Production')
   const [count, setCount] = useState(0)
 
   return (
